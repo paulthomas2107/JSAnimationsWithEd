924e9a8d6dfc01524832957ce40c0b76076aefb8
Paul Thomas <paulthomas2107@gmail.com>
2022-11-18 13:03:31 +0000

    More....

--- app.js
+++ app.js
@@ -13,5 +13,10 @@ links.forEach((link) => {
     // Move the line
     const state = Flip.getState(activeNav)
     link.appendChild(activeNav)
+    Flip.from(state, {
+      duration: 1.25,
+      absolute: true,
+      ease: 'elastic.out(1, 0.5)' 
+    })
   });
 });
