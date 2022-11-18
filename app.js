761e1b6c3acd8ee7ffbf88c64688bfbfa4ddbf9a
Paul Thomas <paulthomas2107@gmail.com>
2022-11-18 13:46:23 +0000

    Complete

--- app.js
+++ app.js
@@ -11,12 +11,41 @@ links.forEach((link) => {
       gsap.to(link, { color: '#385ae0' });
     }
     // Move the line
-    const state = Flip.getState(activeNav)
-    link.appendChild(activeNav)
+    const state = Flip.getState(activeNav);
+    link.appendChild(activeNav);
     Flip.from(state, {
       duration: 1.25,
       absolute: true,
-      ease: 'elastic.out(1, 0.5)' 
+      ease: 'elastic.out(1, 0.5)',
+    });
+  });
+});
+
+// Cards
+const cards = document.querySelectorAll('.card');
+cards.forEach((card, index) => {
+  card.addEventListener('click', () => {
+    // Get state
+    const state = Flip.getState(cards);
+    // Add active class to clicked and make others inactive
+    const isCardActive = card.classList.contains('active');
+    cards.forEach((otherCard, otherIndex) => {
+      otherCard.classList.remove('active');
+      otherCard.classList.remove('is-inactive');
+      if (!isCardActive && index !== otherIndex) {
+        otherCard.classList.add('is-inactive');
+      }
+    });
+    if (!isCardActive) card.classList.add('active');
+    Flip.from(state, {
+      duration: .75,
+      ease: 'expo out',
+      absolute: true
+      /*
+      onComplete: () => {
+        gsap.to('.card p', {y: 500})
+      }
+      */
     })
   });
 });
